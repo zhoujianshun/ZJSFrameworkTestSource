01c528d943101f260a8a80fbe1341f01885569c9
zhoujianshun <jianshun.zhou@mxrcorp.com>
2016-08-24 14:49:05 +0800

    first commit

---
+++ ZJSAdditionOperation.m
@@ -0,0 +1,18 @@
+//
+//  ZJSAdditionOperation.m
+//  ZJSFrameworkTest
+//
+//  Created by 周建顺 on 16/8/24.
+//  Copyright © 2016年 mxrcorp. All rights reserved.
+//
+
+#import "ZJSAdditionOperation.h"
+
+@implementation ZJSAdditionOperation
+
+-(NSInteger)additionOneInteger:(NSInteger)one twoInteger:(NSInteger)two{
+    
+    return one + two;
+}
+
+@end
---
+++ ZJSFrameworkTest.h
@@ -0,0 +1,21 @@
+//
+//  ZJSFrameworkTest.h
+//  ZJSFrameworkTest
+//
+//  Created by 周建顺 on 16/8/24.
+//  Copyright © 2016年 mxrcorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ZJSAdditionOperation.h"
+//#import <ZJSFrameworkTest/ZJSAdditionOperation.h>
+
+//! Project version number for ZJSFrameworkTest.
+FOUNDATION_EXPORT double ZJSFrameworkTestVersionNumber;
+
+//! Project version string for ZJSFrameworkTest.
+FOUNDATION_EXPORT const unsigned char ZJSFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZJSFrameworkTest/PublicHeader.h>
+
+
